c14d84abbc0d090b49141a6617979787dee1e6d2
Alex-PC\Alex <C00003216@louisiana.edu>
2023-03-17 18:33:41 -0500

    add properties to expese model

--- EADA.Core/Domain/Messages/Dto/Expense/ExpenseDto.cs
+++ EADA.Core/Domain/Messages/Dto/Expense/ExpenseDto.cs
@@ -7,7 +7,9 @@ public class ExpenseDto
     public int ExpenseId { get; set; }
     public int ExpenseCategoryId { get; set; }
     public string ExpenseName { get; set; }
-    public decimal CostPerMonth { get; set; }
+    public decimal? CostPerMonth { get; set; }
+    public decimal? CostPerYear { get; set; }
+    public string Description { get; set; }
     public ExpenseType ExpenseType { get; set; }
     public ExpenseCategory ExpenseCategory { get; set; }
 }
